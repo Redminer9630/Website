c2bd2ec078bfe6414627171b6115d715be4ffc90
Redminer9630 <robertchalupka5@gmail.com>
2025-05-10 14:58:14 +0200

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footer = document.createElement('footer');
-    footer.innerHTML = `
-        <span>© 2024 Offizielle Website von Redminer9630 - Alle Rechte vorbehalten.</span>
-    `;
+    footer.innerHTML = ˋ<span>© 2024 Offizielle Website von Redminer9630 - Alle Rechte vorbehalten.</span>`;
     document.body.appendChild(footer);
-
+    const style = document.createElement('style');
+    style.textContent = `
+        footer {
+            background-color: #2c3e50;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+            width: 100%;
+            height: 20px;
+            bottom: 0;
+            left: 0;
+            z-index: 998;
+            position: fixed;
+        }
+    `;
+    document.head.appendChild(style);
+    
     const metaDescription = document.createElement('meta');
     metaDescription.setAttribute('name', 'description');
     metaDescription.setAttribute('content', 'Offizielle Website von Redminer9630.');
