d637bc1b69cad8b8a128cb32a0158840b0f54125
Redminer9630 <robertchalupka5@gmail.com>
2025-05-13 15:56:14 +0200

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -4,8 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(footer);
     const style = document.createElement('style');
     style.textContent = `
-        @font-face { font-family: 'Minecraft', Arial; src: url('minecraft_font.woff'), format('.woff'), url('minecraft_font.ttf'), format('truetype'); }
+        @font-face { font-family: 'Minecraft'; src: url('minecraft_font.woff'), format('.woff'), url('minecraft_font.ttf'), format('truetype'); }
         footer {
+            font-family: 'Minecraft';
             background-color: #2c3e50;
             color: white;
             text-align: center;
