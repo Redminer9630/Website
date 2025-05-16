539706d38850ae0cbd50a2e1129e6d3b99cc65fa
Redminer9630 <robertchalupka5@gmail.com>
2025-05-16 09:10:58 +0200

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footer = document.createElement('footer');
-    footer.innerHTML = `<span>© 2025 Offizielle Website von Redminer9630 - Alle Rechte vorbehalten. v1.6.3 Beta 15.5.25 15:32</span>`;
+    footer.innerHTML = `<span>© 2025 Offizielle Website von Redminer9630 - Alle Rechte vorbehalten. v1.6.4 Beta 16.5.25 09:10</span>`;
     document.body.appendChild(footer);
     const style = document.createElement('style');
     style.textContent = `
