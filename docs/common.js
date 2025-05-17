6e9c146599a5276bb00bcd854942fe6a3f438a8b
Redminer9630 <robertchalupka5@gmail.com>
2025-05-17 10:41:20 +0200

    Update common.js

--- docs/common.js
+++ docs/common.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const version = 'v1.6.6';
-    const versiondate = '16.5.25';
-    const versiontime = '14:08';
+    const version = 'v1.6.7';
+    const versiondate = '17.5.25';
+    const versiontime = '10:40';
     const footer = document.createElement('footer');
     const isMobile = window.innerWidth <= 768;
     footer.innerHTML = isMobile
