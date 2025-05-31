fc5074e23bbae9f7980d081bbaa3ba08ccadff73
Redminer9630 <robertchalupka5@gmail.com>
2025-06-01 01:02:02 +0200

    Update de.js

--- docs/de.js
+++ docs/de.js
@@ -1,9 +1,15 @@
 export default {
-    index_ueber.title: "Ueber Redminer9630",
-    index_ueber.p: "Ich bin ein neuer Minecraft-YouTuber und Streamer, ich habe meinen Kanal vor Kurzem gestartet. Mein Kanal ist noch im Aufbau, und bisher wurden keine Videos veroeffentlicht. Trotzdem werde ich schon in naher Zukunft spannende Inhalte erstellen, und es lohnt sich von Anfang an dabei zu sein.",
-    index_ueber.button: "Mehr ueber mich erfahren",
-    index_mcff.title: "Schreck um Mitternacht - Mincraft Freunde Fanstory",
-    index_mcff.p: "Es war eine stille Nacht im Wald. Vier Freunde – Flo, Gambo, Logo und Ryan – hatten ihr Zelt aufgebaut und sich schlafen gelegt.",
-    index_mcff.button: "Geschichte Lesen",
-    index_band.button: "Jetzt die Bandseite angucken"
+  index_ueber: {
+    title: "Über Redminer9630",
+    p: "Ich bin ein neuer Minecraft-YouTuber und Streamer, ich habe meinen Kanal vor Kurzem gestartet. Mein Kanal ist noch im Aufbau, und bisher wurden keine Videos veröffentlicht. Trotzdem werde ich schon in naher Zukunft spannende Inhalte erstellen, und es lohnt sich von Anfang an dabei zu sein.",
+    button: "Mehr über mich erfahren"
+  },
+  index_mcff: {
+    title: "Schreck um Mitternacht – Minecraft Freunde Fanstory",
+    p: "Es war eine stille Nacht im Wald. Vier Freunde – Flo, Gambo, Logo und Ryan – hatten ihr Zelt aufgebaut und sich schlafen gelegt.",
+    button: "Geschichte lesen"
+  },
+  index_band: {
+    button: "Jetzt die Bandseite angucken"
+  }
 };
