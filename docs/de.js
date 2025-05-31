d8ecc8063123e40c6e99ac3b58350152165a3d5f
Redminer9630 <robertchalupka5@gmail.com>
2025-06-01 00:56:33 +0200

    Update and rename lang.js to de.js

---
+++ docs/de.js
@@ -0,0 +1,9 @@
+export default {
+    index_ueber.title: "Ueber Redminer9630",
+    index_ueber.p: "Ich bin ein neuer Minecraft-YouTuber und Streamer, ich habe meinen Kanal vor Kurzem gestartet. Mein Kanal ist noch im Aufbau, und bisher wurden keine Videos veroeffentlicht. Trotzdem werde ich schon in naher Zukunft spannende Inhalte erstellen, und es lohnt sich von Anfang an dabei zu sein.",
+    index_ueber.button: "Mehr ueber mich erfahren",
+    index_mcff.title: "Schreck um Mitternacht - Mincraft Freunde Fanstory",
+    index_mcff.p: "Es war eine stille Nacht im Wald. Vier Freunde – Flo, Gambo, Logo und Ryan – hatten ihr Zelt aufgebaut und sich schlafen gelegt.",
+    index_mcff.button: "Geschichte Lesen",
+    index_band.button: "Jetzt die Bandseite angucken"
+};
--- docs/lang.js
+++
@@ -1,4 +0,0 @@
-export default {
-  index_ueber.title: "Ueber Redminer9630",
-  // weitere Einträge ...
-};
