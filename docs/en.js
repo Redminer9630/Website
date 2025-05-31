f3c00e3a5aed31d3c3263bb2746bfd70a46b43ea
Redminer9630 <robertchalupka5@gmail.com>
2025-06-01 01:03:34 +0200

    Create en.js

---
+++ docs/en.js
@@ -0,0 +1,15 @@
+export default {
+  index_ueber: {
+    title: "About Redminer9630",
+    p: "I am a new Minecraft YouTuber and streamer. I recently started my channel, which is still under construction, and no videos have been published yet. However, exciting content is coming soon, so it's worth joining from the start.",
+    button: "Learn more about me"
+  },
+  index_mcff: {
+    title: "Midnight Scare – Minecraft Friends Fan Story",
+    p: "It was a quiet night in the forest. Four friends – Flo, Gambo, Logo, and Ryan – had set up their tent and gone to sleep.",
+    button: "Read the story"
+  },
+  index_band: {
+    button: "Visit the band page now"
+  }
+};
