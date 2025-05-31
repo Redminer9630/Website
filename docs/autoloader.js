5badd4bfa4d8c40e3809d2cb82e465c7b07434f1
Redminer9630 <robertchalupka5@gmail.com>
2025-06-01 00:06:25 +0200

    Create autoloader.js

---
+++ docs/autoloader.js
@@ -0,0 +1,10 @@
+Promise.all([
+  import('/lang.js'),
+  import('/elements.js')
+]).then(([lang]) => {
+  document.querySelectorAll('[lang]').forEach(el => {
+    const key = el.getAttribute('lang');
+    const value = lang.default?.[key] || `[[${key}]]`;
+    el.innerText = value;
+  });
+});
