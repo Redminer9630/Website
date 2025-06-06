da04e1b503a15544c3b842a52448bb388b764dc9
Redminer9630 <robertchalupka5@gmail.com>
2025-06-06 19:22:45 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -1,16 +1,9 @@
 Promise.all([
+// Folgende Dateien werden Automatisch geladen wenn im HTML Dokument folgender Script Tag im <head> angegeben wurde:
+// <script type="module" src="autoloader.js"></script>
   import('/docs/lang.js'),
   import('/docs/elements.js'),
   import('/docs/embed.js'),
-  import('/docs/tooltip.js')
-]).then(([langModule]) => {
-  const lang = langModule.default || {};
-
-  document.querySelectorAll('[lang]').forEach(el => { const key = el.getAttribute('lang'); el.innerText = lang[key] || `[[${key}]]`; });
-
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme && savedTheme !== "system") { document.documentElement.setAttribute("data-theme", savedTheme);  } else { document.documentElement.removeAttribute("data-theme"); }
-
-  const savedLang = localStorage.getItem("lang");
-  if (savedLang && savedLang !== "de") { location.reload(); }
-});
+  import('/docs/tooltip.js'),
+  import('/docs/firebase.js')
+]).then(([langModule]) => { const lang = langModule.default || {}; document.querySelectorAll('[lang]').forEach(el => { const key = el.getAttribute('lang'); el.innerText = lang[key] || `[[${key}]]`; }); const savedTheme = localStorage.getItem("theme"); if (savedTheme && savedTheme !== "system") { document.documentElement.setAttribute("data-theme", savedTheme);  } else { document.documentElement.removeAttribute("data-theme"); } const savedLang = localStorage.getItem("lang"); if (savedLang && savedLang !== "de") { location.reload(); } });
