57a22bfc24080e16a0096e46ca6a60748e1083a5
Redminer9630 <robertchalupka5@gmail.com>
2025-06-06 19:28:03 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -7,3 +7,5 @@ Promise.all([
   import('/docs/tooltip.js'),
   import('/docs/firebase.js')
 ]).then(([langModule]) => { const lang = langModule.default || {}; document.querySelectorAll('[lang]').forEach(el => { const key = el.getAttribute('lang'); el.innerText = lang[key] || `[[${key}]]`; }); const savedTheme = localStorage.getItem("theme"); if (savedTheme && savedTheme !== "system") { document.documentElement.setAttribute("data-theme", savedTheme);  } else { document.documentElement.removeAttribute("data-theme"); } const savedLang = localStorage.getItem("lang"); if (savedLang && savedLang !== "de") { location.reload(); } });
+
+document.addEventListener("click",e=>{let el=e.target.closest("[unstable]");if(!el)return;let u=el.getAttribute("href")||el.dataset.href;if(!u)return;e.preventDefault();if(confirm("Du verlässt jetzt den stabilen Bereich dieser Website.\nDiese Seite kann Fehler enthalten oder nicht richtig funktionieren.\nMöchtest du wirklich fortfahren?"))el.getAttribute("target")==="_blank"?open(u,"_blank"):location.href=u});
