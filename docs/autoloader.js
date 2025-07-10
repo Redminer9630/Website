8d3f62ff38a928c9a88764e8d923938e27c52c4a
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-07-10 18:36:34 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -54,7 +54,7 @@ Promise.all([
 		});
 	});
 
-	document.querySelectorAll("img").forEach(img=>{ if(!img.src.endsWith(".webp")) noti(warn, ("Bild sollte in WebP vorliegen:",img.src)); if(img.naturalWidth>800) noti(warn("Bild eventuell zu groß geladen:",img.src,img.naturalWidth+"px")); });
+	document.querySelectorAll("img").forEach(img=>{ if(!img.src.endsWith(".webp")) noti(warn, "Bild sollte in WebP vorliegen:",img.src); if(img.naturalWidth>800) noti(warn, "Bild eventuell zu groß geladen:",img.src,img.naturalWidth+"px"); });
 	noti(info, ("Social Integration empfohlen → Beispiel: https://addthis.com/get/share/"));
 }).catch(err=>{
 	loader.remove();
