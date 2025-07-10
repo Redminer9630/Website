0264bc755120cfff2aa29df3e3f00b82b637aff6
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-07-10 18:16:56 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -21,9 +21,7 @@ window.CommonVersion = {
     time: '11:03'
 };
 
-const loader=document.createElement('div');
-loader.id='loader'; loader.textContent='Lade...'; loader.style.cssText='position:fixed;top:0;left:0;width:100%;background:#000;color:#fff;text-align:center;padding:.5em;font-family:sans-serif;z-index:9999;';
-document.addEventListener("DOMContentLoaded", () => { document.body.appendChild(loader); setTimeout(() => { if (document.getElementById('loader')) loader.remove(); }, 3000); });
+const loader=document.createElement('div');document.addEventListener("DOMContentLoaded", () => {setTimeout(() => { if (document.getElementById('loader')) loader.remove(); }, 3000);});
 Promise.all([
 	import('/js_components/elements.js'),
 	import('/js_components/embed.js'),
