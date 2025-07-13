b31cd6477f50e7beb6c05028c4efb8da3a52bdce
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-07-13 12:11:08 +0200

    Update back_button.js

--- docs/js_components/back_button.js
+++ docs/js_components/back_button.js
@@ -1,28 +1,33 @@
 export function initHeaderButton() {
+  if (location.hostname === 'redminer9630.ddns.net') {
     if (location.pathname === '/' || location.pathname === '/index.html') return;
 
-    const style = document.createElement('style');
-    style.textContent = `
-        .header-link {
-            font-family: 'Mojangles';
-            font-size: 16px;
-            text-decoration: none;
-            padding: 10px 20px;
-            border-radius: 8px;
-            position: absolute;
-            top: 20px;
-            right: 20px;
-            background-color: #f44336;
-            color: white;
-            cursor: pointer;
-        }
-        .header-link:hover { background-color: #e53935; }
-    `;
-    document.head.appendChild(style);
+    const urlParams = new URLSearchParams(location.search);
+    if (urlParams.get('showButton') !== '1') return;
+  }
 
-    const backButton = document.createElement('div');
-    backButton.className = 'header-link';
-    backButton.textContent = 'Zurück';
-    backButton.addEventListener('click', () => history.back());
-    document.body.appendChild(backButton);
+  const style = document.createElement('style');
+  style.textContent = `
+      .header-link {
+          font-family: 'Mojangles';
+          font-size: 16px;
+          text-decoration: none;
+          padding: 10px 20px;
+          border-radius: 8px;
+          position: absolute;
+          top: 20px;
+          right: 20px;
+          background-color: #f44336;
+          color: white;
+          cursor: pointer;
+      }
+      .header-link:hover { background-color: #e53935; }
+  `;
+  document.head.appendChild(style);
+
+  const backButton = document.createElement('div');
+  backButton.className = 'header-link';
+  backButton.textContent = 'Zurück';
+  backButton.addEventListener('click', () => history.back());
+  document.body.appendChild(backButton);
 }
