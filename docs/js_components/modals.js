3a656046db92dfafd9f9430ebbbad0b50ab8c1b6
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-07-15 11:59:29 +0200

    Update modals.js

--- docs/js_components/modals.js
+++ docs/js_components/modals.js
@@ -1,1 +1,11 @@
-
+Modal.create({
+	id: 'infoModal',
+	title: 'Info',
+	content: '<h3>Info</h3><br>Test',
+	width: '400px',
+	height: '250px',
+	anchor: 'center',
+	background: '#333',
+	color: '#fff',
+	draggable: false
+});
