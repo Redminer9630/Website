68e83230f57e1586a17b51014ba7e4a70459021e
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-07-22 21:31:39 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -49,9 +49,10 @@ Promise.all([
 	import('/js_components/back_button.js'),
 	import('/js_components/download.js'),
 	import('/js_components/embed.js'),
-	import('/js_components/mctooltip.js'),
-    import('/js_components/framework.css')
-]).then(([elements, tooltip, firebase, back_button, need_confirm, download, embed, mctip, framecss]) => {
+	import('/js_components/mctooltip.js')
+]).then(([elements, tooltip, firebase, back_button, need_confirm, download, embed, mctip]) => {
+    const css = document.createElement('link');css.rel = 'stylesheet';css.href = '/js_components/framework.css';document.head.appendChild(css);
+    
 	const savedTheme = localStorage.getItem("theme");
 	if (savedTheme === "light" || savedTheme === "dark") document.documentElement.setAttribute("data-theme", savedTheme);
 	else document.documentElement.removeAttribute("data-theme");
