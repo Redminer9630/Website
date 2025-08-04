33344b25afbcecb3c3977bc67799bdd4dde2a269
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-08-04 13:17:59 +0200

    Update back_button.js

--- docs/js_components/back_button.js
+++ docs/js_components/back_button.js
@@ -15,7 +15,7 @@ export function initHeaderButton() {
   shadow.innerHTML = `
     <style>
       :host {all: initial;}
-      button {all: unset;font-family: Mojangles;font-size: 16px;background-color: #f44336;color: white;padding: 10px 20px;border-radius: 8px;cursor: pointer;transition: background-color 0.2s ease, transform 0.2s ease;}
+      button {all: unset;font-family: Arial;font-size: 16px;background-color: #f44336;color: white;padding: 10px 20px;border-radius: 8px;cursor: pointer;transition: background-color 0.2s ease, transform 0.2s ease;}
       button:hover {background-color: #e53935;transform: scale(1.05);}
       @media (max-width: 768px) {button {font-size: 14px;padding: 8px 16px;}}
       @media (max-width: 480px) {button {font-size: 12px;padding: 6px 12px;}}
