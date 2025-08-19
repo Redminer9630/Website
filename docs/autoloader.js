fdeae50decf153ae702d7bd084d718c7b82ede84
Redminer9630 <183291221+Redminer9630de@users.noreply.github.com>
2025-08-19 19:41:02 +0200

    Update autoloader.js

--- docs/autoloader.js
+++ docs/autoloader.js
@@ -12,7 +12,7 @@ if (location.hostname.startsWith("www.")) location.replace(location.href.replace
 window.noti = (type, ...msg) => {const txt = msg.join(" ");const log = console[type] || console.log;log(txt);alert(txt);};
 
 window.debug = false;
-window.CommonVersion = { version: "v1.4.0", key: "1.4.0", date: "17.8.25", time: "17:40" };
+window.CommonVersion = { version: "v1.4.1", key: "1.4.1", date: "19.8.25", time: "19:35" };
 
 const cdnBase = "https://cdn.jsdelivr.net/gh/Redminer9630/Website@t52/docs/js_components/";["back_button.js","mctooltip.js","theme.js"].forEach(file => import(cdnBase + file).catch(e => console.error("Fehler beim Laden:", file, e)));
 
